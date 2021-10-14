f52bcf8f14b537783e6139f96075416f5070c64a
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2021-10-14 10:38:48 +0000

    Update ATMcode.py

--- ATMcode.py
+++ ATMcode.py
@@ -37,58 +37,72 @@ def atmMenu():
         # REQUEST USER TRANSACTION SELECTION
         print ("Type the number associated with the transaction you want to perform and \nPress enter to select")
         print (' \n_______________________________________________________________________________\n  \n')   
-        transaction = int(input("1) Check Balance  2) Make Withdrawal  3) Mobile Money  99) Quit / Log Out\n \n"))
-
-        # CHECK IF USER WANTS TO CHECK BALANCE AND RUN
-        if transaction == 1:
-            # RUN THE BALANCE CHECK FUNCTION
-            crntBalance()
-            pause = input('\n \nPress Enter to return to the Main Menu ')
-            continue
             
-        # CHECK IF USER WANTS TO MAKE A WITHDRAWAL AND RUN
-        elif transaction == 2:
-            # RUN THE WITHDRAWAL FUNCTION AND SAVE ALL OUTPUTS TO THE VARIABLE
-            withdrawn = withdrawUpdate()
-            # RUN THE RECEIPT REQUEST FUNCTION
-            rcptPrint(withdrawn)
-            break
-        
-        # CHECK IF USER WANTS TO MAKE A MOBILE MONEY WITHDRAWAL
-        #elif transaction == 3:
-            # RUN THE MOBILE MONEY FUNCTION
-            #withdrawn = withdrawUpdate()
-           # break
-        
-        # CHECK IF USER WANTS QUIT OR LOG OFF
-        elif transaction == 99:
-            while True:
-                # REQUEST INPUT FROM USER
-                print (' \n_______________________________________________________________________________\n  \n')
-                quitConfirm = str(input('\n \nAre you sure you want to quit? (Y/N):  ')).upper()
-                print (' \n_______________________________________________________________________________\n  \n')
+        try:
+            transaction = int(input("1) Check Balance  2) Make Withdrawal  3) Mobile Money  99) Quit / Log Out\n \n"))
 
-                # IF USER SAYS YES END PROGRAM
-                if quitConfirm == 'Y':
-                    print("""
-                        -----------------------------------------
-                        | Thank you for choosing Tech Seals Bank |
-                        |         Visit us again!                |
-                        -----------------------------------------
-                    """)
-                    exit()
-
-                # IF USER SAYS NO RELOAD THE MAIN MENU
-                elif quitConfirm == 'N':
-                    break
+            # CHECK IF USER WANTS TO CHECK BALANCE AND RUN
+            if transaction == 1:
+                # RUN THE BALANCE CHECK FUNCTION
+                crntBalance()
+                pause = input('\n \nPress Enter to return to the Main Menu ')
+                continue
                 
-                # IF USER RESPONSE IS INVALID ASK FOR A VALID INPUT
-                else:
+            # CHECK IF USER WANTS TO MAKE A WITHDRAWAL AND RUN
+            elif transaction == 2:
+                # RUN THE WITHDRAWAL FUNCTION AND SAVE ALL OUTPUTS TO THE VARIABLE
+                withdrawn = withdrawUpdate()
+                # RUN THE RECEIPT REQUEST FUNCTION
+                rcptPrint(withdrawn)
+                break
+            
+            # CHECK IF USER WANTS TO MAKE A MOBILE MONEY WITHDRAWAL
+            #elif transaction == 3:
+                # RUN THE MOBILE MONEY FUNCTION
+                #withdrawn = withdrawUpdate()
+                # break
+            
+            # CHECK IF USER WANTS QUIT OR LOG OFF
+            elif transaction == 99:
+                while True:
+                    # REQUEST INPUT FROM USER
                     print (' \n_______________________________________________________________________________\n  \n')
-                    print ('Invalid input. Try again')
+                    quitConfirm = str(input('\n \nAre you sure you want to quit? (Y/N):  ')).upper()
                     print (' \n_______________________________________________________________________________\n  \n')
-                    continue
 
+                    # IF USER SAYS YES END PROGRAM
+                    if quitConfirm == 'Y':
+                        print("""
+                            -----------------------------------------
+                            | Thank you for choosing Tech Seals Bank |
+                            |         Visit us again!                |
+                            -----------------------------------------
+                        """)
+                        exit()
+
+                    # IF USER SAYS NO RELOAD THE MAIN MENU
+                    elif quitConfirm == 'N':
+                        break
+                    
+                    # IF USER RESPONSE IS INVALID ASK FOR A VALID INPUT
+                    else:
+                        print (' \n_______________________________________________________________________________\n  \n')
+                        print ('Invalid input. Try again')
+                        print (' \n_______________________________________________________________________________\n  \n')
+                        continue
+
+                continue
+
+            else:
+                print (' \n_______________________________________________________________________________\n  \n')
+                print ('Invalid selection input. Try again')
+                print (' \n_______________________________________________________________________________\n  \n')
+                continue
+        
+        except ValueError:
+            print (' \n_______________________________________________________________________________\n  \n')
+            print ('Invalid selection input. Please input a whole number')
+            print (' \n_______________________________________________________________________________\n  \n')
             continue
 
 
